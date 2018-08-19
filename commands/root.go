e466a3d58ada6c78c739641ee45391ffd9d763b1
tienne <tienne89@gmail.com>
2018-08-20 08:25:33 +0900

    first init

---
+++ commands/root.go
@@ -0,0 +1,33 @@
+package commands
+
+import (
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var (
+	versionGlobalFlag bool
+
+	renderGreenFn    = color.New(color.FgGreen).SprintFunc()
+	renderRedFn      = color.New(color.FgRed).SprintFunc()
+	renderYellowFn   = color.New(color.FgYellow).SprintFunc()
+	renderBlueFn     = color.New(color.FgBlue).SprintFunc()
+	renderCyanBoldFn = color.New(color.FgCyan, color.Bold).SprintFunc()
+)
+
+func init() {
+	RootCmd.Flags().BoolVar(&versionGlobalFlag, "version", false, "Print awless version")
+}
+
+var RootCmd = &cobra.Command{
+	Use:   "awssh",
+	Short: "aws ec2 ssh",
+	Long:  "awssh is a powerful CLI to aws ec2 instance ssh connection",
+	RunE: func(c *cobra.Command, args []string) error {
+		if versionGlobalFlag {
+			//printVersion(c, args)
+			return nil
+		}
+		return c.Usage()
+	},
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/awssh/commands"
+
+func main() {
+	commands.RootCmd.Execute()
+}
